b899472a3ad250aa6e4aadba8abf2bea7564ac7e
Mine <142803161+mminegunduz@users.noreply.github.com>
2024-03-10 17:54:43 +0300

    Moving Object Added.
    ![Ekran görüntüsü 2024-03-10 175226](https://github.com/mminegunduz/CSharpProject/assets/142803161/a8ba7552-febd-46ee-bf3f-cc082662f084)

---
+++ HareketliNesne/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace HareketliNesne
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel6 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel8 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel7 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel9 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel10 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel11 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel12 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel13 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel14 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel15 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            this.timer11 = new System.Windows.Forms.Timer(this.components);
+            this.timer12 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(24, 603);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(1127, 1);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(24, 603);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(196, 80);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(24, 438);
+            this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(196, 59);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(718, 21);
+            this.flowLayoutPanel4.TabIndex = 3;
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(909, 59);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(24, 327);
+            this.flowLayoutPanel5.TabIndex = 3;
+            // 
+            // flowLayoutPanel6
+            // 
+            this.flowLayoutPanel6.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel6.Location = new System.Drawing.Point(218, 497);
+            this.flowLayoutPanel6.Name = "flowLayoutPanel6";
+            this.flowLayoutPanel6.Size = new System.Drawing.Size(715, 21);
+            this.flowLayoutPanel6.TabIndex = 4;
+            // 
+            // flowLayoutPanel8
+            // 
+            this.flowLayoutPanel8.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel8.Location = new System.Drawing.Point(12, 583);
+            this.flowLayoutPanel8.Name = "flowLayoutPanel8";
+            this.flowLayoutPanel8.Size = new System.Drawing.Size(1125, 12);
+            this.flowLayoutPanel8.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(558, 255);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // flowLayoutPanel7
+            // 
+            this.flowLayoutPanel7.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel7.Location = new System.Drawing.Point(338, 385);
+            this.flowLayoutPanel7.Name = "flowLayoutPanel7";
+            this.flowLayoutPanel7.Size = new System.Drawing.Size(595, 21);
+            this.flowLayoutPanel7.TabIndex = 5;
+            // 
+            // flowLayoutPanel9
+            // 
+            this.flowLayoutPanel9.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel9.Location = new System.Drawing.Point(318, 145);
+            this.flowLayoutPanel9.Name = "flowLayoutPanel9";
+            this.flowLayoutPanel9.Size = new System.Drawing.Size(24, 261);
+            this.flowLayoutPanel9.TabIndex = 4;
+            // 
+            // flowLayoutPanel10
+            // 
+            this.flowLayoutPanel10.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel10.Location = new System.Drawing.Point(318, 125);
+            this.flowLayoutPanel10.Name = "flowLayoutPanel10";
+            this.flowLayoutPanel10.Size = new System.Drawing.Size(492, 21);
+            this.flowLayoutPanel10.TabIndex = 6;
+            // 
+            // flowLayoutPanel11
+            // 
+            this.flowLayoutPanel11.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel11.Location = new System.Drawing.Point(807, 125);
+            this.flowLayoutPanel11.Name = "flowLayoutPanel11";
+            this.flowLayoutPanel11.Size = new System.Drawing.Size(24, 198);
+            this.flowLayoutPanel11.TabIndex = 5;
+            // 
+            // flowLayoutPanel12
+            // 
+            this.flowLayoutPanel12.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel12.Location = new System.Drawing.Point(427, 320);
+            this.flowLayoutPanel12.Name = "flowLayoutPanel12";
+            this.flowLayoutPanel12.Size = new System.Drawing.Size(404, 21);
+            this.flowLayoutPanel12.TabIndex = 7;
+            // 
+            // flowLayoutPanel13
+            // 
+            this.flowLayoutPanel13.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel13.Location = new System.Drawing.Point(409, 214);
+            this.flowLayoutPanel13.Name = "flowLayoutPanel13";
+            this.flowLayoutPanel13.Size = new System.Drawing.Size(24, 127);
+            this.flowLayoutPanel13.TabIndex = 6;
+            // 
+            // flowLayoutPanel14
+            // 
+            this.flowLayoutPanel14.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel14.Location = new System.Drawing.Point(409, 196);
+            this.flowLayoutPanel14.Name = "flowLayoutPanel14";
+            this.flowLayoutPanel14.Size = new System.Drawing.Size(312, 21);
+            this.flowLayoutPanel14.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(526, 233);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(116, 81);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 10;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick_1);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 10;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // flowLayoutPanel15
+            // 
+            this.flowLayoutPanel15.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel15.Location = new System.Drawing.Point(697, 214);
+            this.flowLayoutPanel15.Name = "flowLayoutPanel15";
+            this.flowLayoutPanel15.Size = new System.Drawing.Size(24, 40);
+            this.flowLayoutPanel15.TabIndex = 7;
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 10;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Interval = 10;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Interval = 10;
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // timer10
+            // 
+            this.timer10.Interval = 10;
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // timer11
+            // 
+            this.timer11.Interval = 10;
+            this.timer11.Tick += new System.EventHandler(this.timer11_Tick);
+            // 
+            // timer12
+            // 
+            this.timer12.Interval = 10;
+            this.timer12.Tick += new System.EventHandler(this.timer12_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1149, 596);
+            this.Controls.Add(this.flowLayoutPanel15);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.flowLayoutPanel14);
+            this.Controls.Add(this.flowLayoutPanel13);
+            this.Controls.Add(this.flowLayoutPanel12);
+            this.Controls.Add(this.flowLayoutPanel11);
+            this.Controls.Add(this.flowLayoutPanel10);
+            this.Controls.Add(this.flowLayoutPanel9);
+            this.Controls.Add(this.flowLayoutPanel7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel8);
+            this.Controls.Add(this.flowLayoutPanel6);
+            this.Controls.Add(this.flowLayoutPanel5);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel6;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel7;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel9;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel10;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel11;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel12;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel13;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel14;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel15;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.Timer timer10;
+        private System.Windows.Forms.Timer timer11;
+        private System.Windows.Forms.Timer timer12;
+    }
+}
+
---
+++ HareketliNesne/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HareketliNesne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if(pictureBox1.Top<=1)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if (pictureBox1.Left >= 750)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 5;
+            if (pictureBox1.Top >= 350)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 4;
+            if (pictureBox1.Left <=180)
+            {
+                timer4.Stop();
+                timer5.Start();
+            }
+        }
+
+
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if(pictureBox1.Left >=630)
+            {
+                timer6.Stop();
+                timer7.Start();
+
+            }
+        }
+
+        private void timer5_Tick_1(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if (pictureBox1.Top <= 65)
+            {
+                timer5.Stop();
+                timer6.Start();
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 4;
+            if(pictureBox1.Top >= 270)
+            {
+                timer7.Stop();
+                timer8.Start();
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 4;
+            if(pictureBox1.Left<= 265){
+                timer8.Stop();
+                timer9.Start();
+            }
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 4;
+            if (pictureBox1.Top <= 120)
+            {
+                timer9.Stop();
+                timer10.Start();
+            }
+
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 4;
+            if (pictureBox1.Left >= 552)
+            {
+                timer10.Stop();
+                timer11.Start();
+            }
+        }
+
+        private void timer11_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 3;
+            if (pictureBox1.Top >= 213)
+            {
+                timer11.Stop();
+                timer12.Start();
+            }
+
+        }
+
+        private void timer12_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 3;
+            if (pictureBox1.Left <= 478)
+            {
+                timer12.Stop();
+                MessageBox.Show("Tebrikler! Labutları düşürdünüz.","TEBRİKLER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
