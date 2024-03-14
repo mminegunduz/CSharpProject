5af194aa2bd143c91b005d683c1e0e460a236e7d
Mine <142803161+mminegunduz@users.noreply.github.com>
2024-03-14 18:23:55 +0300

    Caesar Encryption Added.
    ![Ekran görüntüsü 2024-03-14 181951](https://github.com/mminegunduz/CSharpProject/assets/142803161/0dbbd64a-8283-4c7c-b5f1-1ca5dc13735e)

---
+++ Sezar_Sifreleme/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sezar_Sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // merhaba m e r h a b a 
+            string veri = "";
+            veri = textBox1.Text;
+            char[] karakterler = veri.ToCharArray();
+            foreach(char eleman in karakterler)
+            {
+                textBox2.Text += Convert.ToChar(eleman + 3).ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sifre = "";
+            sifre = textBox2.Text;
+            char[] karakterler = sifre.ToCharArray();
+            foreach(char eleman in karakterler)
+            {
+                textBox1.Text += Convert.ToChar(eleman - 3).ToString();
+            }
+        }
+    }
+}
